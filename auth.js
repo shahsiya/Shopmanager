76b25f745a37468f45a0d0de492670bf578230b4
shahsiya <77749663+shahsiya@users.noreply.github.com>
2025-05-24 18:20:57 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,16 +1,27 @@
 // auth.js
-import {
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
-} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
+import { getAuth, signInWithEmailAndPassword, signOut as firebaseSignOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 
-import { auth, db } from './firebase-config.js';
-
-export {
-  auth,
-  db,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged
+const firebaseConfig = {
+  apiKey: "AIzaSyBYSoT1yJgt0upA08gkeZoq-FLI0kmqjYk",
+  authDomain: "shopmanager-c9f0b.firebaseapp.com",
+  projectId: "shopmanager-c9f0b",
+  storageBucket: "shopmanager-c9f0b.appspot.com",
+  messagingSenderId: "1029319736818",
+  appId: "1:1029319736818:web:bbfb48553e318955ec3f6b"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+function signOut() {
+  return firebaseSignOut(auth);
+}
+
+export { auth, db, signIn, signOut, onAuthStateChanged };
