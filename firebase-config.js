af670f21cc0c1722377b80bd1ac79bfdc41daf33
shahsiya <77749663+shahsiya@users.noreply.github.com>
2025-05-24 18:09:00 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,4 @@
+// firebase-config.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
