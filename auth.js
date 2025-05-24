46cab5b920eeca938a80b60f8c05571bd6068b13
shahsiya <77749663+shahsiya@users.noreply.github.com>
2025-05-24 18:10:03 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,31 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+// auth.js
 import {
-  getAuth,
-  onAuthStateChanged,
-  signOut,
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+  signOut,
+  onAuthStateChanged
+} from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBYSoT1yJgt0upA08gkeZoq-FLI0kmqjYk",
-  authDomain: "shopmanager-c9f0b.firebaseapp.com",
-  projectId: "shopmanager-c9f0b",
-  storageBucket: "shopmanager-c9f0b.appspot.com",
-  messagingSenderId: "1029319736818",
-  appId: "1:1029319736818:web:bbfb48553e318955ec3f6b"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+import { auth, db } from './firebase-config.js';
 
 export {
   auth,
   db,
-  onAuthStateChanged,
-  signOut, // ← ЭТО ВАЖНО
   signInWithEmailAndPassword,
-  createUserWithEmailAndPassword
+  signOut,
+  onAuthStateChanged
 };
