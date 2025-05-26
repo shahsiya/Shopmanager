086607dc792cfaf6b09b62a5befd4606c8762e0c
shahsiya <77749663+shahsiya@users.noreply.github.com>
2025-05-26 20:44:41 +0300

    Update admin-dashboard.js

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -56,7 +56,7 @@ async function loadAllOrders() {
         </div>
         <p><b>Клиент:</b> ${order.clientEmail}</p>
        <p><b>Описание:</b> ${order.product || 'Нет данных'}</p>
-       <p><b>Клиент:</b> ${order.clientName || 'Нет данных'}</p>
+       <p><b>Имя Клиента:</b> ${order.clientName || 'Нет данных'}</p>
         ${order.createdAt?.toDate().toLocaleString() || 'Неизвестно'}</p>
       `;
       ordersContainer.appendChild(div);
