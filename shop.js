43c65861bc7aeae66450adb135d201db46293d52
shahsiya <77749663+shahsiya@users.noreply.github.com>
2025-06-01 00:09:44 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBYSoT1yJgt0upA08gkeZoq-FLI0kmqjYk",
   authDomain: "shopmanager-c9f0b.firebaseapp.com",
   projectId: "shopmanager-c9f0b",
-  storageBucket: "shopmanager-c9f0b.appspot.com",
+  storageBucket: "shopmanager-c9f0b.firebasestorage.app",
   messagingSenderId: "1029319736818",
   appId: "1:1029319736818:web:bbfb48553e318955ec3f6b"
 };
